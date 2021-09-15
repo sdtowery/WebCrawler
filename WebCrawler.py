a7bbbd08e58ae1665c423eec5c6d5f4e77d49cd0
sdtowery <sam_towery@yahoo.com>
2021-09-14 19:28:55 -0500

    Extracted HTML

--- WebCrawler.py
+++ WebCrawler.py
@@ -1,4 +1,10 @@
-import requests
 
-r = requests.get("https://auburn.edu")
-print(r.text)
+from urllib.request import urlopen
+import bs4
+
+html = urlopen('https://auburn.edu').read()
+soup = bs4.BeautifulSoup(html, features="html.parser")
+for script in soup(["script", "style"]):
+    script.extract()
+
+print(soup.prettify())
