38fbe5222cc741d509f3cc5b3107087c606794d2
sdtowery <sam_towery@yahoo.com>
2021-09-15 19:41:18 -0500

    Implemented webcrawl function

--- WebCrawler.py
+++ WebCrawler.py
@@ -2,25 +2,40 @@ import validators
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
+from validators.url import url
 
-def iterative_deepening_search(url, depth):
+
+def ids():
+    pass
+
+
+def webcrawl(url):
     # Documentation for BeautifulSoup found here:
     # https://www.crummy.com/software/BeautifulSoup/bs4/doc/
-    html = urlopen(user_url).read()
+    if not validators.url(url):
+        return
+
+    html = urlopen(url).read()
     soup = BeautifulSoup(html, features="html.parser")
+    print(soup.original_encoding)
 
     # Reference for removing JavaScript code:
     # https://stackoverflow.com/questions/328356/extracting-text-from-html-file-using-python
     for script in soup(["script", "style"]):
         script.extract()
 
-    # print(soup.prettify())
+    content = soup.prettify()
+    write_file(content, url)
 
-    url_list = list()
+    url_list = []
     for link in soup.find_all('a'):
         url_list.append((link.get('href')))
 
     return url_list
+
+
+def write_file(content, url):
+    pass
 
 
 def unigram_extractor(links):
@@ -38,5 +53,6 @@ while True:
 
 depth = int((input("Enter a maximum depth: ")))
 
-url_list = iterative_deepening_search(user_url, depth)
-unigram_extractor(url_list)
+url_list = webcrawl(user_url)
+print(url_list)
+# unigram_extractor(url_list)
