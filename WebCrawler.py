94a838efc49afc0d8b685ce540630ed1d64b4a48
HStog <haydenstog3@gmail.com>
2021-09-15 21:41:31 -0500

    Added IDS function. Filtered out duplicate urls

--- WebCrawler.py
+++ WebCrawler.py
@@ -5,9 +5,28 @@ from urllib.request import urlopen
 from validators.url import url
 
 
-def ids():
-    pass
-
+def ids(url, max_depth):
+    visited = []
+    url_list = []
+    url_list.append(url)
+    # Run until url_list is empty
+    while url_list:
+        if not url in visited:
+            visited.append(url)
+            # Get children urls
+            temp_list = webcrawl(url)
+            # Pop current url from stack
+            url_list.pop()
+            # Add temp_list to url_list if not None
+            if temp_list != None:
+                url_list += temp_list
+            # Set url to top of stack
+            url = url_list[-1]
+        else:
+            # Pop current url from stack and set url to top of stack 
+            url_list.pop()
+            url = url_list[-1]
+            
 
 def webcrawl(url):
     # Documentation for BeautifulSoup found here:
@@ -17,7 +36,6 @@ def webcrawl(url):
 
     html = urlopen(url).read()
     soup = BeautifulSoup(html, features="html.parser")
-    print(soup.original_encoding)
 
     # Reference for removing JavaScript code:
     # https://stackoverflow.com/questions/328356/extracting-text-from-html-file-using-python
@@ -29,7 +47,10 @@ def webcrawl(url):
 
     url_list = []
     for link in soup.find_all('a'):
-        url_list.append((link.get('href')))
+        url = link.get('href')
+        if url == None: continue
+        # Strip '/' from the end of the url to avoid duplicates
+        url_list.append(url.rstrip('/'))
 
     return url_list
 
@@ -53,6 +74,5 @@ while True:
 
 depth = int((input("Enter a maximum depth: ")))
 
-url_list = webcrawl(user_url)
-print(url_list)
+url_list = ids(user_url, depth)
 # unigram_extractor(url_list)
