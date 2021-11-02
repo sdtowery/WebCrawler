e45f9f240e87f0dcfeadbcb2a0e0e3ef98ee3f30
HStog <haydenstog3@gmail.com>
2021-11-02 03:42:19 -0500

    Got evolutionary_cycle to a "working" state
    Still need to look into why numbers converge so fast to 0.94. Probably due to the calculate_fitness function but not sure.

--- Assignment_3/simpleSteadyStateGA.py
+++ Assignment_3/simpleSteadyStateGA.py
@@ -9,13 +9,11 @@ import os
 import random
 import sys
 import math
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 
 #
 #  A Simple Steady-State, Real-Coded Genetic Algorithm       
 #
-    
+
 class anIndividual:
     def __init__(self, specified_chromosome_length):
         self.chromosome = []
@@ -36,6 +34,17 @@ class anIndividual:
 
     def print_individual(self, i):
         print("Chromosome "+str(i) +": " + str(self.chromosome) + " Fitness: " + str(self.fitness))
+
+    def _is_valid_operand(self, other):
+        return (hasattr(other, "fitness"))
+    def __eq__(self, other):
+        if not self._is_valid_operand(other):
+            return NotImplemented
+        return (self.fitness == other.fitness)
+    def __lt__(self, other):
+        if not self._is_valid_operand(other):
+            return NotImplemented
+        return (self.fitness < other.fitness)
       
 class aSimpleSteadyStateGA:
     def __init__(self, population_size, chromosome_length, mutation_rate):
@@ -63,11 +72,9 @@ class aSimpleSteadyStateGA:
                 worst_individual = i
         return worst_individual
     
-    def get_n_worst_fit_individuals(self):
-        worst_fitness = 999999999.0  # For Maximization
-        worst_individuals = []
-        pass
-    
+    def sort_ascending_fitness(self):
+        self.population.sort()
+
     def get_best_fitness(self):
         best_fitness = -99999999999.0
         best_individual = -1
@@ -78,28 +85,47 @@ class aSimpleSteadyStateGA:
         return best_fitness
         
     def evolutionary_cycle(self, num_parents):
+        self.sort_ascending_fitness()
+
         parents = []
         for i in range(num_parents):
             # Grab two random individuals from the population
             indiviual_A, indiviual_B = random.sample(range(0, self.population_size), 2)
             # Use the best one of the two as a parent
-            parents.append(max(self.population[indiviual_A].fitness, self.population[indiviual_B].fitness))
-        kid = self.get_worst_fit_individual()
-        
+            A_fitness = self.population[indiviual_A].fitness
+            B_fitness = self.population[indiviual_B].fitness
+            parent = indiviual_A if A_fitness >= B_fitness else indiviual_B
+            parents.append(parent)
+
         chromosome_probs = []
         for chrom_num in range(self.chromosome_length):
-            one_count = 0
+            ones_count = 0
             # Sum all of the 1s of a chromosome
-            for parent in range(num_parents):
-                current_chromosome = self.population[parents[parent]].chromosome[chrom_num]
+            for parent in parents:
+                current_chromosome = self.population[parent].chromosome[chrom_num]
                 if current_chromosome == 1:
-                    one_count += 1
+                    ones_count += 1
             # Divide total 1s by total num of parents to get the probability of passing down 1 to the kid
-            chromosome_prob = one_count / num_parents
+            chromosome_prob = ones_count / num_parents
+            # print(f"Ones Count: {ones_count}  |  Number of Parents: {num_parents}  |  Chromosome Probability: {chromosome_prob}\n\n")
             chromosome_probs.append(chromosome_prob)
-        
-        self.population[kid].calculate_fitness()
-       
+
+        for kid in range(num_parents):
+            for chrom_num in range(self.chromosome_length):
+                # Chromosome = 1 if a random number is less than or equal to the probability
+                random_num = random.uniform(0, 1)
+                self.population[kid].chromosome[chrom_num] = \
+                    1 if random_num <= chromosome_probs[chrom_num] else 0
+                # print(f"Probability: {chromosome_probs[chrom_num]}  |  Random Number: {random_num}  |  Chromosome Value: {self.population[kid].chromosome[chrom_num]}")
+                # If random number is less than or equal to the probability then a mutation occurs
+                # Mutation flips the bit, i.e., 0 -> 1 | 1 -> 0
+                random_num = random.uniform(0, 1)
+                if random_num <= self.mutation_amt:
+                    self.population[kid].chromosome[chrom_num] = \
+                        self.population[kid].chromosome[chrom_num] ^ (1 << 0)
+                # print(f"Mutation Rate: {self.mutation_amt}  |  Random Number: {random_num}  |  Chromosome Value: {self.population[kid].chromosome[chrom_num]}\n\n")
+            self.population[kid].calculate_fitness()
+
     def print_population(self):
         for i in range(self.population_size):
             self.population[i].print_individual(i)
@@ -121,27 +147,25 @@ MaxEvaluations = 15000
 # mu_amt  = float(sys.argv[2])
 
 PopSize = 44
-mu_amt = 0.0001
+mu_amt = 0.01
+num_parents = (int) (PopSize / 2)
 
 simple_steadystate_ga = aSimpleSteadyStateGA(PopSize,ChromLength,mu_amt)
 
 simple_steadystate_ga.generate_initial_population()
-#simple_steadystate_ga.print_population()
+# simple_steadystate_ga.print_population()
 
+for i in range(MaxEvaluations-PopSize+1):
+    simple_steadystate_ga.evolutionary_cycle(num_parents)
+    print("At Iteration: " + str(i))
+    simple_steadystate_ga.print_population()
+    if (simple_steadystate_ga.get_best_fitness() >= 0.99754):
+        break
 
-
-# for i in range(MaxEvaluations-PopSize+1):
-#     simple_steadystate_ga.evolutionary_cycle()
-#     if (i % PopSize == 0):
-#         print("At Iteration: " + str(i))
-#         simple_steadystate_ga.print_population()
-#     if (simple_steadystate_ga.get_best_fitness() >= 0.99754):
-#         break
-
-# print("\nFinal Population\n")
-#simple_steadystate_ga.print_population()
+print("\nFinal Population\n")
+# simple_steadystate_ga.print_population()
 simple_steadystate_ga.print_best_max_fitness()
-print("Function Evaluations: " + str(PopSize+i))
+# print("Function Evaluations: " + str(PopSize+i))
 
 
 
