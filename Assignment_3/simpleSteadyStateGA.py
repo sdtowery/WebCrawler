63fcd109ade0b6d4b30c07668f4b2ca6326a4865
sdtowery <sam_towery@yahoo.com>
2021-11-03 23:39:18 -0500

    adds functionality to create a csv file containing the best fit chromosomes

--- Assignment_3/simpleSteadyStateGA.py
+++ Assignment_3/simpleSteadyStateGA.py
@@ -11,20 +11,21 @@ import sys
 import math
 
 #
-#  A Simple Steady-State, Real-Coded Genetic Algorithm       
+#  A Simple Steady-State, Real-Coded Genetic Algorithm
 #
+
 
 class anIndividual:
     def __init__(self, specified_chromosome_length):
         self.chromosome = []
-        self.fitness    = 0
+        self.fitness = 0
         self.chromosome_length = specified_chromosome_length
-        
+
     def randomly_generate(self):
         for i in range(self.chromosome_length):
             self.chromosome.append(random.randint(0, 1))
         self.fitness = 0
-    
+
     # TODO: Get better fitness function
     def calculate_fitness(self):
         # x2y2 = self.chromosome[0]**2 + self.chromosome[1]**2
@@ -32,23 +33,29 @@ class anIndividual:
         for i in range(self.chromosome_length):
             x2y2 += self.chromosome[i]**2
             # print(f"Chromosome value: {self.chromosome[i]}  |  x2y2: {x2y2}")
-        self.fitness = 0.5 + (math.sin(math.sqrt(x2y2))**2 - 0.5) / (1+0.001*x2y2)**2
+        self.fitness = 0.5 + (math.sin(math.sqrt(x2y2)) **
+                              2 - 0.5) / (1+0.001*x2y2)**2
         # print(f"Fitness: {self.fitness}\n")
+
     def print_individual(self, i):
         # print("Chromosome " +str(i) + " - Fitness: " + str(self.fitness))
-        print("Chromosome "+str(i) +": " + str(self.chromosome) + " Fitness: " + str(self.fitness))
+        print("Chromosome "+str(i) + ": " + str(self.chromosome) +
+              " Fitness: " + str(self.fitness))
 
     def _is_valid_operand(self, other):
         return (hasattr(other, "fitness"))
+
     def __eq__(self, other):
         if not self._is_valid_operand(other):
             return NotImplemented
         return (self.fitness == other.fitness)
+
     def __lt__(self, other):
         if not self._is_valid_operand(other):
             return NotImplemented
         return (self.fitness < other.fitness)
-      
+
+
 class aSimpleSteadyStateGA:
     def __init__(self, population_size, chromosome_length, mutation_rate):
         if (population_size < 2):
@@ -58,19 +65,19 @@ class aSimpleSteadyStateGA:
         self.chromosome_length = chromosome_length
         self.mutation_amt = mutation_rate
         self.population = []
-        
+
     def generate_initial_population(self):
         for i in range(self.population_size):
             individual = anIndividual(self.chromosome_length)
             individual.randomly_generate()
             individual.calculate_fitness()
             self.population.append(individual)
-    
+
     def get_worst_fit_individual(self):
         worst_fitness = 999999999.0  # For Maximization
         worst_individual = -1
         for i in range(self.population_size):
-            if (self.population[i].fitness < worst_fitness): 
+            if (self.population[i].fitness < worst_fitness):
                 worst_fitness = self.population[i].fitness
                 worst_individual = i
         return worst_individual
@@ -83,14 +90,15 @@ class aSimpleSteadyStateGA:
                 best_fitness = self.population[i].fitness
                 best_individual = i
         return best_fitness
-        
+
     def evolutionary_cycle(self, num_parents):
         self.population.sort()
 
         parents = []
         for i in range(num_parents):
             # Grab two random individuals from the population
-            indiviual_A, indiviual_B = random.sample(range(0, self.population_size), 2)
+            indiviual_A, indiviual_B = random.sample(
+                range(0, self.population_size), 2)
             # Use the best one of the two as a parent
             A_fitness = self.population[indiviual_A].fitness
             B_fitness = self.population[indiviual_B].fitness
@@ -129,7 +137,7 @@ class aSimpleSteadyStateGA:
     def print_population(self):
         for i in range(self.population_size):
             self.population[i].print_individual(i)
-    
+
     def get_best_max_fitness(self):
         best_fitness = -999999999.0  # For Maximization
         best_individual = -1
@@ -141,4 +149,5 @@ class aSimpleSteadyStateGA:
 
     def print_best_max_fitness(self):
         best_fitness, best_individual = self.get_best_max_fitness(self)
-        print("Best Indvidual: ",str(best_individual)," ", self.population[best_individual].chromosome, " Fitness: ", str(best_fitness))
+        print("Best Indvidual: ", str(best_individual), " ",
+              self.population[best_individual].chromosome, " Fitness: ", str(best_fitness))
--- Assignment_3/wrapper.py
+++ Assignment_3/wrapper.py
@@ -1,31 +1,50 @@
 import csv
+from os import write, makedirs
 from os.path import exists
 from simpleSteadyStateGA import aSimpleSteadyStateGA
 
 # Params: algorithm object, algorithm type
 # Returns: feature mask, best fitness
+
+
 def run_algorithm(ssga, algorithm_type):
     if algorithm_type == "SSGA":
         num_parents = 2
     elif algorithm_type == "SEDA":
-        num_parents = (int) (PopSize / 2)
+        num_parents = (int)(PopSize / 2)
     else:
         print("Invalid algorithm type...")
         return
-    
+
     ssga.generate_initial_population()
     for i in range(MaxEvaluations-PopSize+1):
         ssga.evolutionary_cycle(num_parents)
         print("At Iteration: " + str(i))
         # ssga.print_population()
-        
+
         # Found optimized feature mask.
-        # TODO: Replace 0.99754 with value signifying optimized feature mask has been found 
+        # TODO: Replace 0.99754 with value signifying optimized feature mask has been found
         if (ssga.get_best_fitness() >= 0.99754):
             break
     best_fitness, best_individual = ssga.get_best_max_fitness()
     feature_mask = ssga.population[best_individual].chromosome
     return feature_mask, best_fitness
+
+# Params: a row to insert into the csv file
+
+
+def write_to_csv(file_name, row):
+    try:
+        if exists(file_name):
+            file = open(file_name, 'a', newline='')
+        else:
+            file = open(file_name, 'x', newline='')
+
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(row)
+        file.close()
+    except Exception as err:
+        print(f"Error: {err}")
 
 
 # ----- Wrapper Config ----- #
@@ -40,15 +59,34 @@ MaxEvaluations = 15000
 PopSize = 44
 mu_amt = 0.01
 
-column_headers = list(range(ChromLength))
-column_headers.insert(0,"Run #")
+ssga_feature_mask_filename = ssga_directory + feature_mask_filename
 
 for i in range(algorithm_runs):
     print(f"----- SSGA Run #{i+1} -----")
-    ssga = aSimpleSteadyStateGA(PopSize,ChromLength,mu_amt)
+    ssga = aSimpleSteadyStateGA(PopSize, ChromLength, mu_amt)
     ssga_feature_mask, ssga_best_fitness = run_algorithm(ssga, "SSGA")
+
     # TODO: Create file and write ssga_feature_mask as a new row
-    
+    if i == 0:
+        try:
+            # Create directory for ssga csv file if it doesn't already exist
+            if not exists(ssga_directory):
+                makedirs(ssga_directory)
+        except OSError:
+            print('Error: Creating directory. ' + ssga_directory)
+        except Exception as err:
+            print(f"Error: {err}")
+
+        # column_headers = list(range(ChromLength))
+        column_headers = ["Run #"]
+        # column_headers.insert(0, "Run #")
+        for j in range(95):
+            column_headers.append(j)
+
+        write_to_csv(ssga_feature_mask_filename, column_headers)
+    row = [i]
+    row.extend(ssga_feature_mask)
+    write_to_csv(ssga_feature_mask_filename, row)
 
 
 # seda = aSimpleSteadyStateGA(PopSize,ChromLength,mu_amt)
