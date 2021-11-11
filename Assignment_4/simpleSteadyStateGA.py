660a83783cf53b7d1ccf50347e20b9d6ae8270a9
sdtowery <sam_towery@yahoo.com>
2021-11-11 14:29:54 -0600

    Initialized Assignment 4 and refactored some code.

--- Assignment_4/simpleSteadyStateGA.py
+++ Assignment_4/simpleSteadyStateGA.py
@@ -11,60 +11,12 @@ import csv
 import random
 import sys
 import math
+from helper import write_feature_mask_dataset
 from HTML_Malware import HTML_Malware
 
 #
 #  A Simple Steady-State, Real-Coded Genetic Algorithm
 #
-
-
-# writes the feature masked csv file
-#
-# Params: feature_mask
-
-
-def write_feature_mask_dataset(feature_mask):
-    base_dataset = "HTML_malware_dataset.csv"
-    feature_mask_dataset = "feature_mask_dataset.csv"
-
-    try:
-        if exists(feature_mask_dataset):
-            remove(feature_mask_dataset)
-
-        feature_mask_file = open(feature_mask_dataset, 'x', newline='')
-        base_dataset_file = open(base_dataset, 'r')
-
-        csv_reader = csv.reader(base_dataset_file)
-        csv_writer = csv.writer(feature_mask_file)
-        i = 0
-        for row in csv_reader:
-            # copy the first row of the base dataset to the new dataset
-            if i == 0:
-                csv_writer.writerow(row)
-                i += 1
-                continue
-
-            # multiply the row by the feature mask and add it to new dataset
-            row_data = []
-            col_num = 0
-            for j in range(len(row)):
-                if j == 0 or j == 1:
-                    # copy the first two elements to the new row
-                    row_data.append(row[j])
-                else:
-                    # multiple the row element by the feature mask and add it to new row
-                    feature = float(row[j])
-                    feature = feature * float(feature_mask[col_num])
-                    row_data.append(feature)
-                    col_num += 1
-
-            i += 1
-            csv_writer.writerow(row_data)
-
-        feature_mask_file.close()
-        base_dataset_file.close()
-    except Exception as err:
-        print(f"Error: {err}")
 
 
 def get_fitness(chromosome):
